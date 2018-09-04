bb449eeb5839c0cbb55e7fad63683ba7f89546df
Tynan Barlow <tynan@noukod.org>
2018-09-04 14:15:09 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +8,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+  index = user_input.to_i
+  index = index - 1
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def update_array_at_with(board, index, value = 'X')
+  board[index] = value
+end
