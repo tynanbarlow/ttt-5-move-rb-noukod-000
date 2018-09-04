279b9542e0da02d0119e71263cbc5f1ca1901197
Tynan Barlow <tynan@noukod.org>
2018-09-04 14:20:43 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,7 +10,7 @@ end
 # code your input_to_index and move method here!
 def input_to_index(user_input)
   return user_input.to_i - 1
-  
+
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
